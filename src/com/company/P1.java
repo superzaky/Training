f578a519eca131526957d148c33d00590f63c4f6
superzaky <superkytoz@gmail.com>
2020-06-05 23:45:24 +0200

    Refactor solution of problem 1.

--- src/com/company/P1.java
+++ src/com/company/P1.java
@@ -30,21 +30,39 @@ public class P1 implements Solution {
         //Betere oplossing zonder loop
         //This solution is based on the following observation:
         //1 + 2 + ... + n = n*(n+1)/2
-        int nr = 1000;
-        nr--;
-        int x3 = nr/3;
-        int x5 = nr/5;
-        int x15 = nr/15;
+//        int nr = 1000;
+//        nr--;
+//        int x3 = nr/3;
+//        int x5 = nr/5;
+//        int x15 = nr/15;
+//
+//        long sum1 = 3*x3*(x3+1);
+//        long sum2 = 5*x5*(x5+1);
+//        // There is a caveat! Both of these count multiples of both 15. It counts/adds them twice one for each.
+//        //So in order to balance that out, we subtract once in the following line:
+//        //long sum = (sum1+sum2-sum3)/2;
+//        long sum3 = 15*x15*(x15+1);
+//
+//        long sum = (sum1+sum2-sum3)/2;
 
-        long sum1 = 3*x3*(x3+1);
-        long sum2 = 5*x5*(x5+1);
-        // There is a caveat! Both of these count multiples of both 15. It counts/adds them twice one for each.
-        //So in order to balance that out, we subtract once in the following line:
-        //long sum = (sum1+sum2-sum3)/2;
-        long sum3 = 15*x15*(x15+1);
+        // My own solution
+        float sum1 = calculateSumOfMultiplier(3);
+        float sum2 = calculateSumOfMultiplier(5);
+        float sum3 = calculateSumOfMultiplier(15);
 
-        long sum = (sum1+sum2-sum3)/2;
+        long sum = (long) (sum1+sum2-sum3);
 
         return String.valueOf(sum);
+    }
+
+    public float calculateSumOfMultiplier(int multiplier){
+        // based on formula s = n/2 * (a + (a*n))
+        long n = 999 / multiplier;
+        float q = n/ 2.0f;
+        return  q* ( multiplier + multiply( n, multiplier));
+    }
+
+    private long multiply(long n, int multiplier) {
+        return n * multiplier;
     }
 }
