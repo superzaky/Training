edf9287ca9f08692c1164e902d3551230aefaa9a
superzaky <superkytoz@gmail.com>
2026-05-04 22:59:44 +0200

    wip: p1

--- haskell/app/p1.hs
+++ haskell/app/p1.hs
@@ -1,4 +1,10 @@
 module Main where
+
+calculateSumOfMultiplier::Integer -> Float
+calculateSumOfMultiplier multiplier = 
+  let n  = 999 / multiplier
+  Num q = n / 2.0
+
 main = do
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
